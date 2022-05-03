90c46af505b077bb5126d4b2f995793bcdd76143
Yash-Singh1 <saiansh2525@gmail.com>
2022-05-03 16:14:38 -0700

    fix: ts error on _app Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/dist/shared/lib/router/router';
 import { ReactNode } from 'react';
 import DynamicTina  from '../.tina/components/TinaDynamicProvider'
 
-const App = ({ Component, pageProps }: AppProps & { Component: JSX.Element }) => {
+const App = ({ Component, pageProps }: AppProps & { Component: any }) => {
   return (
     <DynamicTina>
         <Component {...pageProps} />
