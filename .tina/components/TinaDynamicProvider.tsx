d75c09292e5ceb051f4ce5273eeee2245987adfc
Yash-Singh1 <saiansh2525@gmail.com>
2022-05-03 16:13:18 -0700

    chore: migrate .tina/components to typescript

--- .tina/components/TinaDynamicProvider.tsx
+++ .tina/components/TinaDynamicProvider.tsx
@@ -1,8 +1,9 @@
 import dynamic from "next/dynamic";
 const TinaProvider = dynamic(() => import("./TinaProvider"), { ssr: false });
 import { TinaEditProvider } from "tinacms/dist/edit-state";
+import type { ReactNode } from "react";
 
-const DynamicTina = ({ children }) => {
+const DynamicTina = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <TinaEditProvider editMode={<TinaProvider>{children}</TinaProvider>}>
--- .tina/components/TinaProvider.tsx
+++ .tina/components/TinaProvider.tsx
@@ -1,5 +1,6 @@
+import type { ReactNode } from "react";
 import TinaCMS from "tinacms";
-import { tinaConfig } from "../schema.ts";
+import { tinaConfig } from "../schema";
 
 // Importing the TinaProvider directly into your page will cause Tina to be added to the production bundle.
 // Instead, import the tina/provider/index default export to have it dynamially imported in edit-moode
@@ -7,7 +8,8 @@ import { tinaConfig } from "../schema.ts";
  *
  * @private Do not import this directly, please import the dynamic provider instead
  */
-const TinaProvider = ({ children }) => {
+const TinaProvider = ({ children }: { children: ReactNode }) => {
+  // @ts-ignore
   return <TinaCMS {...tinaConfig}>{children}</TinaCMS>;
 };
 
